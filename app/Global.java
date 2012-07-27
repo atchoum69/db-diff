5687bcb868bd0c31615895aa1984643261be5475
atchoum69 <lefort.cedric@gmail.com>
2012-07-27 13:12:27 +0200

    simplification du chargement de donnes au démarrage de l'application

--- app/Global.java
+++ app/Global.java
@@ -1,39 +1,27 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Construct;
-import org.yaml.snakeyaml.constructor.Constructor;
-
 import models.Database;
-import play.*;
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.libs.Yaml;
+
+import com.avaje.ebean.Ebean;
 
 public class Global extends GlobalSettings {
 
 	@Override
 	public void onStart(Application app) {
-		// Check if the database is empty
-		if(Database.find.findRowCount() == 0) {
+		// Verifie si la base est vide
+	  if(Ebean.find(Database.class).findRowCount() == 0) {
 			Logger.info("Chargement des donnees initial dans la base de donnees.");
 			
-			// insertion manuelle via le fichier initial-data.yml
-			Yaml yaml = new Yaml();
-			try {
-				ArrayList<Database> databases = (ArrayList<Database>) yaml.load(new FileInputStream(new File("app/initial-data.yml")));
-				for (Database database : databases) {
-					database.save();
-				}
-			} catch (FileNotFoundException e) {
-				Logger.error(e.getMessage());
-			}
+			// chargement des donneees
+			Map<String,List<Object>> all = (Map<String,List<Object>>) Yaml.load("initial-data.yml");
+			
+			// Insertion des donnees dans la base
+      Ebean.save(all.get("databases"));
 			
 		} else {
 			Logger.info("Base de donnees deja chargee.");
